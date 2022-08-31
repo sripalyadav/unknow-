d5faa3ec8299018068b7659b8c68c59352807e26
Sripalyadav <kookatlasripalyadav@gmail.com>
2022-08-31 12:19:15 +0530

    new

---
+++ src/main/java/com/sripal/UsersManagementSystemRestApiProject2Application.java
@@ -0,0 +1,13 @@
+package com.sripal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersManagementSystemRestApiProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersManagementSystemRestApiProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sripal/UsersManagementSystemRestApiProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sripal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersManagementSystemRestApiProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
